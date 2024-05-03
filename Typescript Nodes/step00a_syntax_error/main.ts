b9a508a10b0627a1303fbf9b9907e06041c7240f
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-03 02:49:45 -0700

    learn Typescript

---
+++ Typescript Nodes/step00a_syntax_error/main.ts
@@ -0,0 +1,4 @@
+// This program intentionally includes syntax errors, such as misspelled keywords, missing punctuation, or incorrect variable declarations, to demonstrate how the compiler or interpreter responds to such issues.By encountering and analyzing these errors, programmers can gain a better understanding of the language's syntax rules and learn to identify and correct errors effectively
+ 
+leet Name = "Peter"; // syntax error
+console.log(Name);
