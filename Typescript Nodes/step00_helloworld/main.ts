755d3ef3047b4369a8e43be980717c36700af20d
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-03 02:48:02 -0700

    learn Typescript

---
+++ Typescript Nodes/step00_helloworld/main.ts
@@ -0,0 +1,4 @@
+ // Traditionally, when beginning to program, writing HelloWorld is the first task undertaken by beginner programmers and a sign that they're about to embark on their programming journey 
+
+let beginning = "HelloWorld!";
+console.log(beginning);
