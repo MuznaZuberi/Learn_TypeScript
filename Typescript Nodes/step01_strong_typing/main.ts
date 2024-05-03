3fbc1f8a6f61d31e0bd26d21c68db5aabba56ce8
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-05-03 02:58:05 -0700

    learn Typescript

---
+++ Typescript Nodes/step01_strong_typing/main.ts
@@ -0,0 +1,24 @@
+//  In addition, strong typing makes it easier to read the code and improves its stability through explicit definitions of predicted data types for variables or functions. This program will allow developers to gain a deeper understanding of the principles underlying strong typing and its role in promoting sound software development practices
+
+let My_Education : string = '"Graduation"'; // Strong Typing
+console.log("My_Education : ", My_Education);
+
+let Favrt_Number : number = 9;
+console.log("Favrt_Number :", Favrt_Number); // Strong Typing
+
+let Identification : boolean = true;
+console.log("Identification : ", Identification); // Strong Typing
+
+
+
+
+//type inference
+
+let Country = "Pakistan";
+Country = "Malaysia";
+
+let Num = 19;
+Num = 2;
+
+let Verification  = false;
+Verification = true;
